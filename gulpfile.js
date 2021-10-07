6c49ee3ea00d485d895b2f4164dd6ccc7bea9abb
Sébastien Gérard <me@sgerard.be>
2021-10-07 07:22:24 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const del = require('del');
+const merge = require("merge-stream");
+
+gulp.task('clean', () => {
+    return del([
+        'css/main.css',
+    ]);
+});
+
+gulp.task('styles', () => {
+    return gulp.src('scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./build/css/'));
+});
+
+gulp.task('copy-resources', function() {
+  return merge([
+      gulp.src('./*.html').pipe(gulp.dest('./build/')),
+  ]);
+});
+
+gulp.task('watch', () => {
+    gulp.watch('scss/**/*.scss', (done) => {
+        gulp.series(['clean', 'styles'])(done);
+    });
+});
+
+gulp.task('default', gulp.series(['clean', 'styles', 'copy-resources']));
